2050d98b041a35b513ba682edb4207475ec53b9e
kawacukennedy <kawacukent@gmail.com>
2025-11-21 21:00:39 +0200

    Update Providers to include ThemeProvider

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,12 +3,15 @@
 import { Provider } from 'react-redux';
 import { store } from '@/store';
 import Modal from './Modal';
+import ThemeProvider from './ThemeProvider';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <Provider store={store}>
-      {children}
-      <Modal />
+      <ThemeProvider>
+        {children}
+        <Modal />
+      </ThemeProvider>
     </Provider>
   );
 }

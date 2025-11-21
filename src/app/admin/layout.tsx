e3cd2d2f25fbfe4e3c0d98732b9f178e393953d7
kawacukennedy <kawacukent@gmail.com>
2025-11-21 21:08:53 +0200

    Add admin layout

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,22 @@
+import Navbar from '@/components/Navbar';
+import Sidebar from '@/components/Sidebar';
+import Footer from '@/components/Footer';
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen bg-background_light dark:bg-background_dark">
+      <Navbar />
+      <div className="pt-16 flex">
+        <Sidebar />
+        <main className="flex-1">
+          {children}
+        </main>
+      </div>
+      <Footer />
+    </div>
+  );
+}

767c796e91a084679725ef5dc7715df92c502dce
kawacukennedy <kawacukent@gmail.com>
2025-11-21 22:26:50 +0200

    docs: add comment to Redux store configuration

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,4 @@
+// Redux store configuration for TerraFund
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import uiReducer from './slices/uiSlice';

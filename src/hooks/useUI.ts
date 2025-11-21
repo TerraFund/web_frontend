ce04d37a8d79e7da2078699f9560744ea0eea894
kawacukennedy <kawacukent@gmail.com>
2025-11-21 22:06:57 +0200

    feat: enhance useUI hook with toast notification functions

--- src/hooks/useUI.ts
+++ src/hooks/useUI.ts
@@ -1,19 +1,27 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '@/store';
-import { toggleDarkMode, setSidebarOpen, openModal, closeModal } from '@/store/slices/uiSlice';
+import { toggleDarkMode, setSidebarOpen, openModal, closeModal, showToast, hideToast } from '@/store/slices/uiSlice';
 
 export const useUI = () => {
   const dispatch = useDispatch();
-  const { darkMode, sidebarOpen, modalOpen, modalContent } = useSelector((state: RootState) => state.ui);
+  const { darkMode, sidebarOpen, modalOpen, modalContent, toast } = useSelector((state: RootState) => state.ui);
+
+  const displayToast = (message: string, type: 'success' | 'error' | 'info' = 'info', duration = 3000) => {
+    dispatch(showToast({ message, type, duration }));
+    setTimeout(() => dispatch(hideToast()), duration);
+  };
 
   return {
     darkMode,
     sidebarOpen,
     modalOpen,
     modalContent,
+    toast,
     toggleDarkMode: () => dispatch(toggleDarkMode()),
     setSidebarOpen: (open: boolean) => dispatch(setSidebarOpen(open)),
-    openModal: (content: string) => dispatch(openModal(content)),
+    openModal: (content: React.ReactNode) => dispatch(openModal(content)),
     closeModal: () => dispatch(closeModal()),
+    showToast: displayToast,
+    hideToast: () => dispatch(hideToast()),
   };
 };

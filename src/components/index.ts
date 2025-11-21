f062e66bef1b359571f4afa846c52c60bbf68df7
kawacukennedy <kawacukent@gmail.com>
2025-11-21 20:49:54 +0200

    Add components index file for organized exports

---
+++ src/components/index.ts
@@ -0,0 +1,27 @@
+// Layout Components
+export { default as Navbar } from './Navbar';
+export { default as Sidebar } from './Sidebar';
+export { default as Footer } from './Footer';
+
+// UI Components
+export { default as Button } from './Button';
+export { default as Input } from './Input';
+export { default as Select } from './Select';
+export { default as Modal } from './Modal';
+export { default as Toast } from './Toast';
+export { default as Loading } from './Loading';
+export { default as Error } from './Error';
+export { Card, CardHeader, CardContent, CardFooter } from './Card';
+
+// Feature Components
+export { default as Map } from './Map';
+export { default as LandCard } from './LandCard';
+export { default as LandFilters } from './LandFilters';
+export { default as ProposalModal } from './ProposalModal';
+export { default as ContractModal } from './ContractModal';
+export { default as PaymentModal } from './PaymentModal';
+export { default as ReviewModal } from './ReviewModal';
+export { default as NotificationDropdown } from './NotificationDropdown';
+
+// Providers
+export { default as Providers } from './Providers';
